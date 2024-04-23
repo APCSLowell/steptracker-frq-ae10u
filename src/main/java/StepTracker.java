66506e76428613939417b036ae708041d2754757
ae10u <142628398+ae10u@users.noreply.github.com>
2024-04-23 10:59:58 -0700

    Update StepTracker.java

--- src/main/java/StepTracker.java
+++ src/main/java/StepTracker.java
@@ -5,35 +5,33 @@ public class StepTracker
  private int totalSteps;
  private int numDays;
  private int numActiveDays;
- public StepTracker(int threshold)
- {
+ public StepTracker(int threshold){
  minSteps = threshold;
  totalSteps = 0;
  numDays = 0;
  numActiveDays = 0;
  }
- public void addDailySteps(int steps)
- {
- totalSteps += steps;
- numDays++;
- if (steps >= minSteps)
- {
- numActiveDays++;
- }
- }
+ public void addDailySteps(int steps){
+   totalSteps += steps;
+   numDays++;
+   if (steps >= minSteps)
+   {
+     numActiveDays++;
+   }
+   }
  public int activeDays()
  {
- return numActiveDays;
+   return numActiveDays;
  }
  public double averageSteps()
  {
- if (numDays == 0)
- {
- return 0.0;
- }
- else
- {
- return (double) totalSteps / numDays;
- }
+   if (numDays == 0)
+   {
+   return 0.0;
+   }
+   else
+   {
+   return (double) totalSteps / numDays;
+   }
  }
 } 
